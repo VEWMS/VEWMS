dee1949a47047d5da9011fa97124901c9de8f74f
white19 <c50419001@gmail.com>
2014-06-09 15:36:36 +0800

    index test connect to vm server

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
-<html>
-<body>
 <?php
-     print_r ( libvirt_version() );
+     if($conn = libvirt_connect('qemu+ssh://cdpalibvirt@jupiter03.cdpa.tw/system', false))
+	echo 'QAQ';
+     else
+	echo '1919';
+     $doms = libvirt_list_domains($conn);
 ?>
-</body>
-</html>
