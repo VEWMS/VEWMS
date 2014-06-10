9a57f9bb39d21396b6b806f70486021261fc6e90
white19 <c50419001@gmail.com>
2014-06-10 22:28:51 +0800

    add index page QAQ

---
+++ public/index.php
@@ -0,0 +1,50 @@
+<html>
+<head></head>
+<style type="text/css">
+body {
+    color: purple;
+    background-color: #C991FF }
+#main_index{
+width:100px;
+height:100px;
+position:absolute;
+top:0;
+left:0;
+background: #E7CDFF;
+}
+
+#tool{
+width:100%;
+height:30px;
+position:absolute;
+top:0;
+left:100px;
+background: #FFFFFF;
+}
+
+#footer {
+width:100%;
+height:50px;
+position:absolute;
+bottom:0;
+left:0;
+background:#8000FA;
+}
+</style>
+<body>
+<div id ="main_index">
+header 0w0/
+</div>
+
+<div id = "tool">
+tool 0w0/
+</dir>
+
+</div>
+<div id="footer">
+footer 0w0/
+
+</div>
+</body>
+</html>
+
