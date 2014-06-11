0e92818598eef856ac23a9505bfe6cd05255455b
white19 <c50419001@gmail.com>
2014-06-11 12:58:36 +0800

    add link page

--- public/OVS_Manager/OVS_main_page.php
+++ public/OVS_Manager/OVS_main_page.php
@@ -2,7 +2,21 @@
 <style type="text/css">
 </style>
 <body>
-我是 OVS_main_page.php
+<center>
+Hostname:JupiterTest.cdpa.tw</br>
+<select>
+ <option>請選擇網路介面</option>
+ <option>te-1/1/1</option>
+</select>
+</br>
+<select>
+ <option>網路介面速度調整</option>
+ <option>100M</option>
+ <option>1G</option>
+</select>
+</br>
+<button>變更</button>
+</center>
 </body>
 
 </html>
---
+++ public/link.php
@@ -0,0 +1,8 @@
+<html>
+<body>
+<a href="https://github.com/VEWMS/VEWMS">Source code</a></br>
+這裡有我們的開源碼可以參考和使用，或者歡迎一起維護</br>
+<a href="http://www.nsysu.edu.tw/">國立中山大學首頁</a></br>
+<a href="http://www.cdpa.nsysu.edu.tw/">CDPA首頁</a></br>
+</body>
+</html>
