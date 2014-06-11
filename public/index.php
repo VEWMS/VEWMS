ae1ae2e653c598130fccafc39c1dee4ee69cbea7
white19 <c50419001@gmail.com>
2014-06-11 09:01:40 +0800

    19 index page down

--- public/index.php
+++ public/index.php
@@ -1,25 +1,86 @@
 <html>
-<head></head>
+<head>
+
+<script type="text/javascript" src="http://code.jquery.com/jquery-1.11.1.min.js"></script>
+
+<script type="text/JavaScript">
+$(document).ready(function() {
+	$( "#myframe" ).load( "welcom.html" );
+////////////////////////
+//VS
+///////////////////////
+	$("#ovs_link").click(function(){
+		$( "#myframe" ).load( "test.html" );
+	});
+/////////////////////
+//friend
+//////////////////////
+
+});
+</script>
+
+</head>
 <style type="text/css">
 body {
     color: purple;
     background-color: #C991FF }
-#main_index{
-width:100px;
-height:100px;
+
+#title{
+width:250px;
+height:180px;
 position:absolute;
-top:0;
-left:0;
-background: #E7CDFF;
+top:30;
+left:30;
+color:yellow;
 }
 
-#tool{
-width:100%;
-height:30px;
+#main_index{
+width:85px;
+height:85px;
 position:absolute;
-top:0;
-left:100px;
-background: #FFFFFF;
+top:90;
+left:120;
+background-color: rgba(0,0,0,.5);
+}
+#b{
+width:60px;
+height:60px;
+position:absolute;
+top:130;
+left:90;
+background-color: rgba(0,0,0,.5);
+}
+#c{
+width:140px;
+height:140px;
+position:absolute;
+top:20;
+left:20;
+background-color: rgba(0,0,0,.5);
+}
+
+
+#tool{
+width:75%;
+height:28px;
+position:absolute;
+top:40px;
+right:0px;
+background-color: rgba(255,255,255,.5);
+border-radius:5px 0px 0px 5px;
+}
+#btnSet{
+left:10px;
+}
+
+#myframe{
+width:80%;
+height:80%;
+position:absolute;
+top:100px;
+right:0;
+background-color: rgba(255,255,255,.5);
+border-radius:10px 0px 0px 10px;
 }
 
 #footer {
@@ -30,19 +91,41 @@ bottom:0;
 left:0;
 background:#8000FA;
 }
+.btn{
+width:160px;
+margin-left: 5px;
+background:#611BA3;
+border-radius:0px 10px 0px 10px
+
+}
 </style>
 <body>
-<div id ="main_index">
-header 0w0/
-</div>
 
+
+<div id ="main_index">
+</div>
+<div id ="b">
+</div>
+<div id ="c">
+</div>
+<div id = "title">
+<font size="5">Virtual Equipment</font></br></br>
+<font size="5">Webpage</font></br></br>
+<font size="5">Management System</font></br>
+</div>
 <div id = "tool">
-tool 0w0/
-</dir>
+<div name="btnSet">   
+	<button class="btn" id="vm_link"><font color=white>虛擬機器管理介面</font></button>
+	<button class="btn" id="ovs_link"><font color=white>虛擬交換器管理介面</font></button>
+	<button class="btn" id="friend"><font color=white>友善連結</font></button>
+</div>
+</div>
+<div id = "myframe">
 
 </div>
 <div id="footer">
-footer 0w0/
+<center>維護人員:White19</center>
+<center>國立中山大學 CDPA 宿舍米蟲</center>
 
 </div>
 </body>
